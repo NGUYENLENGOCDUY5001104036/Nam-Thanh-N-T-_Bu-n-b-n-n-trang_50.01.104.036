2906e64c9b59102fa9c4dc41bc3035e99d797135
NGUYENLENGOCDUY5001104036 <nguyenlengocduy0312@gmail.com>
2024-12-01 23:21:56 +0700

    Add files via upload

--- Nam Thanh Nữ Tú_Buôn Bán Nữ Trang_50.01.104.036/assets/js/shop.js
+++
@@ -1,7 +0,0 @@
-function searchUser() {
-    let valueSearchInput = document.getElementById("search").value
-    let userSearch = users.filter(value => {
-        return value.name.toUpperCase().includes(valueSearchInput.toUpperCase())
-    })
-    renderUser(userSearch)
-}
